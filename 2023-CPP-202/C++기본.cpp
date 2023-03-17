312f6685b7fe50f350559fff72de2ca27ac33b05
LacteaMariposa <s2218@e-mirim.hs.kr>
2023-03-17 09:41:33 +0900

    개행객체 endl

--- 2023-CPP-202/C++기본.cpp
+++ 2023-CPP-202/C++기본.cpp
@@ -1,11 +1,12 @@
 //cout 객체 사용
 #include <iostream>
+using namespace std;
 
 int main(void) {
 
 	// << <= 출력연산자
-	
-	std::cout << "Hello C++\n";
+	//endl : 개행 (줄바꿈)
+	cout << "Hello C++" << endl;
 
 	return 0;
 }
