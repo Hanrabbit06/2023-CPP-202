89e4607a7d454299c28770f8db747602e7d5baf7
Hanrabbit06 <s2218@e-mirim.hs.kr>
2023-10-17 14:36:09 +0900

    auto를 통하여 자료형으로 자동으로 맞춰준다.

--- 2023-CPP-202/문법/범위기반 for문.cpp
+++ 2023-CPP-202/문법/범위기반 for문.cpp
@@ -4,8 +4,13 @@ using namespace std;
 
 int main(void) 
 {
-	int a[] = { 1,2,3,4,5 };
-	for (int x : a) {
+	int a[4] = { 1,2,3,4 };
+
+	//누가봐도 a의 자료형은 int이다.
+	auto b = 4;
+
+	//x의 자료형은 int로 자동설정
+	for (auto x : a) {
 		cout << x << endl;
 	}
 	
