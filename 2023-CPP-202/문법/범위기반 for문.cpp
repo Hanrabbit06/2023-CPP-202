7d8a76d3856b5e97c2952cb58992f8e4429840e2
Hanrabbit06 <s2218@e-mirim.hs.kr>
2023-10-17 14:31:23 +0900

    범위기반 for문

--- 2023-CPP-202/문법/범위기반 for문.cpp
+++ 2023-CPP-202/문법/범위기반 for문.cpp
@@ -1,14 +1,12 @@
 ﻿#include <iostream>
-#include <map>
-#include <string>
 
 using namespace std;
 
 int main(void) 
 {
 	int a[] = { 1,2,3,4,5 };
-	for (int i = 0; i < 5; i++) {
-		cout << a[i]<< endl;
+	for (int x : a) {
+		cout << x << endl;
 	}
 	
 	return 0;
