3e863e8c015b52f120c72c7b3152e65786f33935
Hanrabbit06 <s2218@e-mirim.hs.kr>
2023-10-17 14:27:23 +0900

    전통적인 for문

---
+++ 2023-CPP-202/문법/범위기반 for문.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main(void) 
+{
+	int a[] = { 1,2,3,4,5 };
+	for (int i = 0; i < 5; i++) {
+		cout << a[i]<< endl;
+	}
+	
+	return 0;
+}
